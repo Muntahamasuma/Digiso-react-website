482d8f0f26ef1590671828757c4bdcb97efbbf9b
Muntahamasuma <knockmasuma@gmail.com>
2024-10-06 02:05:18 +0600

    Added 'Feature Services' section

--- src/Componnets/HeaderText.jsx
+++ src/Componnets/HeaderText.jsx
@@ -1,11 +1,14 @@
 import 'animate.css';
 
-const HeaderText = ({title, description}) => {
+const HeaderText = ({title, description, leftAlignText, leftAlignDescription}) => {
     return (
         <div className="animate__animated animate__slideInLeft font">
         <h1 className="text-3xl lg:text-5xl text-center font-bold w-1/2 m-auto">{title}
           </h1>
+        <h1 className="text-3xl lg:text-5xl text-left font-bold">{leftAlignText}
+          </h1>
           <p className='py-6 font-medium text-[#6F6F6F] text-center'>{description}</p>
+          <p className='w-3/4 font-medium text-[#6F6F6F] text-left'>{leftAlignDescription}</p>
     </div>
     );
 };
--- src/section/Feature_Services.jsx
+++ src/section/Feature_Services.jsx
@@ -1,8 +1,10 @@
+import HeaderText from "../Componnets/HeaderText";
 
 
 const Feature_Services = () => {
     return (
         <div>
+            
             <div className="dark:bg-gray-100 dark:text-gray-900">
   <div className="container grid grid-cols-12 mx-auto">
     <div
@@ -14,14 +16,11 @@ const Feature_Services = () => {
         backgroundSize: "cover",
       }}
     >
-      <div className="flex flex-col items-center p-8 py-12 text-center dark:text-gray-800">
-        <span>12 June</span>
-        <h1 className="py-4 text-5xl font-bold">
-          Lorem, ipsum dolor sit amet consectetur adipisicing.
-        </h1>
-        <p className="pb-6">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, a!
-        </p>
+      <div className="flex flex-col space-y-10 p-8 dark:text-gray-800">
+      <HeaderText
+            leftAlignText={"Our Feature Services"}
+            leftAlignDescription={"Etiam sed odio dictum, tempus velit non, sollicitudin neque. Donec quis aliquet velit. Aliquam convallis dapibus purus."}
+            ></HeaderText>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
@@ -36,20 +35,21 @@ const Feature_Services = () => {
         </svg>
       </div>
     </div>
-    <div className="flex flex-col col-span-12 p-6 divide-y lg:col-span-6 lg:p-10 dark:divide-gray-300">
-      {Array(3)
-        .fill()
-        .map((_, index) => (
-          <div key={index} className="pt-6 pb-4 space-y-2">
-            <span>12 June</span>
-            <h1 className="text-3xl font-bold">Lorem ipsum dolor sit.</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, a!</p>
+    {/*  */}
+    <div className="flex flex-col col-span-12 px-4 md:px-6 lg:px-8 divide lg:col-span-6 lg:p-10 dark:divide-gray-300 gap-10 ">
+      
+          <div className="pb-4  border-2 border-gray-200">
+
+           <div className="flex justify-around items-center space-y-10">
+           <div><img src="https://i.ibb.co.com/rcXGHqs/Icon.png" alt="" /></div>
+           <div className="w-1/2"><h1 className="text-3xl font-bold font">Planning</h1>
+            <p className="font text-lg">Etiam sed odio dictum, tempus velit non, sollicitudin nutoeque. Donec quis aliquet velit.</p>
             <a
               rel="noopener noreferrer"
               href="#"
               className="inline-flex items-center py-2 space-x-2 text-sm dark:text-violet-600"
             >
-              <span>Read more</span>
+              <span className="font text-xl hover:underline text-black font-bold">Read more</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
@@ -62,9 +62,66 @@ const Feature_Services = () => {
                   clipRule="evenodd"
                 ></path>
               </svg>
-            </a>
-          </div>
-        ))}
+            </a></div>
+           </div>
+               </div>
+          <div className="pb-4 border-2 border-gray-200">
+
+           <div className="flex justify-around items-center space-y-10">
+           <div><img src="https://i.ibb.co.com/0jpSbnM/image-5.png" alt="" /></div>
+           <div className="w-1/2"><h1 className="text-3xl font-bold font">Branding</h1>
+            <p className="font text-lg">Etiam sed odio dictum, tempus velit non, sollicitudin nutoeque. Donec quis aliquet velit.</p>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="inline-flex items-center py-2 space-x-2 text-sm dark:text-violet-600"
+            >
+              <span className="font text-xl hover:underline text-black font-bold">Read more</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </a></div>
+           </div>
+               </div>
+          <div className="pb-4 border-2 border-gray-200">
+
+           <div className="flex justify-around items-center space-y-10">
+           <div><img src="https://i.ibb.co.com/R73zTXk/image-6.png" alt="" /></div>
+           <div className="w-1/2"><h1 className="text-3xl font-bold font">Marketing</h1>
+            <p className="font text-lg">Etiam sed odio dictum, tempus velit non, sollicitudin nutoeque. Donec quis aliquet velit.</p>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="inline-flex items-center py-2 space-x-2 text-sm dark:text-violet-600"
+            >
+              <span className="font text-xl hover:underline text-black font-bold">Read more</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </a></div>
+           </div>
+               </div>
+
+          
+     
     </div>
   </div>
 </div>
